f0faf4c57515cc3cc09cd612df4c9c27c1a6d9e8
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-08-03 15:03:26 +0300

    Added search function

--- pages/Movies/index.js
+++ pages/Movies/index.js
@@ -1,13 +1,43 @@
+import { useState } from "react";
 import Link from "next/link";
 import style from "./styles.module.css";
+
 function Movies({ movies }) {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchResults, setSearchResults] = useState(null);
+
+  const handleSearch = async () => {
+    if (searchQuery.trim() === "") {
+      setSearchResults(null);
+    } else {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=ccb0a8566b23ab43471cda53fed3d9e7&language=en-US&query=${searchQuery}&page=1&include_adult=false`
+      );
+      const data = await response.json();
+      setSearchResults(data);
+    }
+  };
+
   return (
     <section className={style.section}>
       <div className={style.movieTitle}>
         <h2>Movies</h2>
       </div>
+
+      <div className={style.inputWrapper}>
+        <input
+          className={style.input}
+          type="text"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button className={style.btn} onClick={handleSearch}>
+          Search
+        </button>
+      </div>
+
       <div className={style.cardContainer}>
-        {movies?.results?.map((item) => {
+        {(searchResults?.results || movies?.results)?.map((item) => {
           return (
             <div key={item.id} className={style.card}>
               <Link href={`Movies/${item.id}`}>
@@ -33,7 +63,7 @@ export default Movies;
 
 export async function getStaticProps() {
   const response = await fetch(
-    `https://api.themoviedb.org/3/movie/popular?api_key=ccb0a8566b23ab43471cda53fed3d9e7&language=en-US&page`
+    `https://api.themoviedb.org/3/movie/popular?api_key=ccb0a8566b23ab43471cda53fed3d9e7&language=en-US&page=1`
   );
   const data = await response.json();
   return {
